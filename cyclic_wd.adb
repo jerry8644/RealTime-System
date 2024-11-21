b0d7b4c7f292cd2ce86848aab2b90c2196b80d42
jerry <jerry8644@gmail.com>
2024-11-22 02:45:50 +0800

    q2_unfinished

--- cyclic_wd.adb
+++ cyclic_wd.adb
@@ -7,12 +7,29 @@ use Ada.Text_IO;
 -- add packages to use random number generator
 
 procedure cyclic_wd is
-   Message    : constant String := "Cyclic scheduler with watchdog";
-   -- change/add your declarations here
-   Next_Time  : Time := Clock;
-   d          : Duration := 1.0;
-   Start_Time : Time := Clock;
+   Message       : constant String := "Cyclic scheduler with watchdog";
+   Next_Time     : Time := Clock;
+   d             : Duration := 1.0;
+   Start_Time    : Time := Clock;
    Timer_Expired : Boolean := False;
+   
+   task type Watchdog is
+      entry Start;
+   end Watchdog;
+
+   task body Watchdog is
+   begin
+      accept Start;
+      delay 0.5;
+      if Timer_Expired then
+         Put_Line ("Warning: f3 execution time limit exceeded.");
+      end if;
+   end Watchdog;
+
+   type Watchdog_Ptr is access all Watchdog;
+
+
+
 
    procedure f1 is
       Message : constant String := "f1 executing, time is now";
@@ -30,39 +47,18 @@ procedure cyclic_wd is
 
    procedure f3 is
       Message : constant String := "f3 executing, time is now";
+      T1 : Watchdog_Ptr := new Watchdog;
    begin
       Put (Message);
       Put_Line (Duration'Image (Clock - Start_Time));
-      -- add a random delay here
-      --  delay 1.5;  -- Simulating f3 taking longer than expected
-      Timer_Expired := False;
+      Timer_Expired := True;
+      T1.Start;
+      delay 1.0; --TODO random time 
+      T1 := null;
+      Timer_Expired := False;        
    end f3;
 
-   task Watchdog is
-      entry Start;
-      entry Stop;
-   end Watchdog;
 
-   task body Watchdog is
-   begin
-      loop
-         select
-            accept Start do
-               Timer_Expired := True;
-               -- Start a delay to monitor the task execution time
-               delay 2.0;
-               if Timer_Expired then
-                  Put_Line ("Warning: f3 execution time limit exceeded.");
-               end if;
-            end Start;
-         or
-            accept Stop do
-               Timer_Expired := True;
-               Put_Line ("Watchdog stopped");
-            end Stop;
-         end select;
-      end loop;
-   end Watchdog;
 
 begin
    loop
@@ -70,9 +66,7 @@ begin
       f1;
       f2;
       delay until Next_Time + 0.5;
-      Watchdog.Start;
       f3;
-      --  Watchdog.Stop;
       Next_Time := Next_Time + d;
    end loop;
 end cyclic_wd;
